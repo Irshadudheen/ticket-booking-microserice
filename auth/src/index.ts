e0f2ca3c9a8f49802bbbcc63df607345f2a526d2
Irshadudheen <irshadudheen.p10@gmail.com>
2024-10-24 14:39:39 +0530

    chore(skaffold):ready know

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,4 +2,5 @@ import express,{json} from 'express'
 const app = express()
 const port = 3000
 app.use(json())
-app.listen(port,()=>console.log('the server is running on 3000'))
+
+app.listen(port,()=>console.log('the server is running on 3000!!!'))
