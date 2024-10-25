4ec423132647ff0c171408244135cb13bc9b4481
Irshadudheen <irshadudheen.p10@gmail.com>
2024-10-25 09:05:42 +0530

    feat(ingress):feature add ingress gate-way

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express,{json} from 'express'
 const app = express()
+import cors from 'cors'
 const port = 3000
 app.use(json())
-
+app.use(cors())
+app.get('/api/users/getuserinfo',(req,res)=>{res.send('hi you are their')})
 app.listen(port,()=>console.log('the server is running on 3000!!!'))
