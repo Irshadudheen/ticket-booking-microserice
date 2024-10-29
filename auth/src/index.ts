b4efde859ac6bca9f418e8d904c545c09e235293
Irshadudheen <irshadudheen.p10@gmail.com>
2024-10-30 01:19:21 +0530

    feat(error-handler):perfect and standard error handler middleware has been created

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,19 @@
 import express,{json} from 'express'
+import { currentUserRouter,singinRouter,singoutRouter,singupRouter } from './routes/index'
+import { errorhandler } from './middlewares/error-handler'
+import 'express-async-errors'
 const app = express()
 import cors from 'cors'
+import { NotFoundError } from './errors/not-found-error'
 const port = 3000
 app.use(json())
 app.use(cors())
-app.get('/api/users/getuserinfo',(req,res)=>{res.send('hi you are their')})
+app.use(currentUserRouter)
+app.use(singinRouter)
+app.use(singoutRouter)
+app.use(singupRouter)
+app.all('*',async()=>{
+    throw new NotFoundError();
+})
+app.use(errorhandler as express.ErrorRequestHandler)
 app.listen(port,()=>console.log('the server is running on 3000!!!'))
---
+++ auth/src/routes/index.ts
@@ -0,0 +1,5 @@
+import { currentUserRouter } from "./current-user";
+import {singinRouter} from './singin'
+import { singupRouter } from "./singup";
+import { singoutRouter } from "./singout";
+export {currentUserRouter,singinRouter,singoutRouter,singupRouter}
