6946b33ce1e53ca3c0b3cd26816130d0f3a28c40
Irshadudheen <irshadudheen.p10@gmail.com>
2024-10-30 02:07:34 +0530

    feat(mongoose):add mongoose and depl the mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express,{json} from 'express'
 import { currentUserRouter,singinRouter,singoutRouter,singupRouter } from './routes/index'
 import { errorhandler } from './middlewares/error-handler'
 import 'express-async-errors'
+import mongoose from 'mongoose'
 const app = express()
 import cors from 'cors'
 import { NotFoundError } from './errors/not-found-error'
@@ -15,5 +16,16 @@ app.use(singupRouter)
 app.all('*',async()=>{
     throw new NotFoundError();
 })
+
 app.use(errorhandler as express.ErrorRequestHandler)
-app.listen(port,()=>console.log('the server is running on 3000!!!'))
+
+const start = async()=>{
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('conneted to mongodb')
+    } catch (error) {
+        console.error(error)
+    }
+    app.listen(port,()=>console.log('the server is running on 3000!!!'))
+}
+start()
