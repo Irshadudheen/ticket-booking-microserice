026721c7d06b0f70c9321edb4ad70c69360ed22b
Irshadudheen <irshadudheen.p10@gmail.com>
2024-10-30 17:15:20 +0530

    fix(middleware):bug fix in error middlewawre

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,1 +1,37 @@
-import mongoose from "mongoose";
+import {model,Model,Document,Schema} from 'mongoose';
+
+
+interface UserAttrs {
+ 
+    email:string,
+    password:string,
+}
+
+interface UserModel extends Model<UserDoc>{
+    build(attrs:UserAttrs):UserDoc;
+}
+
+interface UserDoc extends Document{
+    email:string,
+    password:string,
+}
+
+const userScheema = new Schema({
+  
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+});
+
+
+userScheema.statics.build = (attrs:UserAttrs)=>{
+    return new User(attrs)
+}
+
+const User = model<UserDoc,UserModel>('User',userScheema)
+export { User };
--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connetion-error';
+import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validateRequest';
+
 
 const router = express.Router();
 
@@ -13,7 +15,7 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
-],
+],validateRequest,
 async (req: Request, res: Response)=> {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -22,9 +24,17 @@ async (req: Request, res: Response)=> {
     }
     
     const { email, password } = req.body;
-    console.log('Creating user');
-    throw new DatabaseConnectionError()
-     res.send({}); 
+    const existingUser = await User.findOne({email});
+
+    if(existingUser){
+        console.log('Email in use')
+        res.send({message:'email in use'})
+        return
+    }
+    const user = User.build({email,password})
+    await user.save()
+    
+     res.status(201).send(user); 
 });
 
 export { router as singupRouter };
